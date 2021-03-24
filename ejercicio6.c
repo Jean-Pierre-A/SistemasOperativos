659f2a5e41999393951f8aae9f6e61a20b786c51
Jean-Pierre-A <jean.agudelo@upb.edu.co>
2021-03-24 08:08:17 -0500

    Ejercicio 6 Terminado

---
+++ ejercicio6.c
@@ -0,0 +1,39 @@
+ #include <unistd.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+
+ 
+ int main(int argc, char *argv[]) 
+ {
+    int nroHijo = 0;
+    pid_t pid_hijo, pid_hijo2;
+    int status;
+    printf("El pid del programa padre es: %d\n",(int)getpid());
+  
+
+    for(int i = 1; i <= 3; i++)
+    {
+       pid_hijo = fork();
+       nroHijo ++;
+
+       if(pid_hijo == 0)
+       {
+           printf("HIJO %d\n", nroHijo);
+          break;
+       }
+       else{
+          wait(NULL);
+       }
+    }
+
+   int Fact = 1;
+    for(int i = 1; i <= 10; i++)
+    { 
+       if(pid_hijo == 0)
+       {
+       Fact *= i;
+       printf(" Hijo %d  = FACT [%d] %d \n",nroHijo, i , Fact);
+       }
+    }
+   
+  }
